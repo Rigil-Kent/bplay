4885ec74c423608f8025b478f7eb2c61c79839f3
Bryan Bailey <bcbailey84@gmail.com>
2020-04-24 21:39:06 -0400

    implemented volume control

--- bplay.js
+++ bplay.js
@@ -3,10 +3,37 @@ const play = document.getElementById('play');
 const stop = document.getElementById('stop');
 const camera = document.getElementById('cam');
 const expand = document.getElementById('expand');
+const mute = document.getElementById('mute');
 const progress = document.getElementById('progress');
+const volume = document.getElementById('volume');
 const timestamp = document.getElementById('timestamp');
 const media = navigator.mediaDevices;
 
+const classPrefix = 'bplay_';
+const poster = '';
+const defaultVideoWidth = 480;
+const defaultVideoHeight = 270;
+const defaultAudioWidth = 400;
+const defaultAudioHeight = 34;
+const startVolume = 0.8;
+let autoplay = false;
+let autoRewind = true;
+let hideVideoControlsOnLoad = false;
+let hideVideoControlsOnPause = false;
+let controlsTimeout = 1500;
+let hideVolumeOnTouchDevices = true;
+let tempVolume = 0;
+
+
+
+
+function loadVideo(src) {
+    video.srcObject = src;
+}
+
+function setVideoPoster(src) {
+    video.poster = src;
+}
 
 // Play & Pause video
 function toggleVideoStatus() {
@@ -29,13 +56,38 @@ function updatePlayIcon() {
     }
 }
 
+function updateVolume() {
+    volume.value = video.volume;
+    if (volume.value > 0.6) {
+        mute.innerHTML = '<i class="fas fa-volume-up"></i>';
+    } else if (volume.value > 0) {
+        mute.innerHTML = '<i class="fas fa-volume-down"></i>';
+    } else {
+        mute.innerHTML = '<i class="fas fa-volume-mute"></i>';
+    }
+}
 
 // Update progress & timestamp
 function updateProgress() {
     progress.value = (video.currentTime / video.duration) * 100;
 
+    if (video.ended) {
+        if (autoRewind) {
+            video.currentTime = 0;
+        }
+    }
+
+    let hours = undefined;
     // Get minutes 
     let mins = Math.floor(video.currentTime / 60);
+    
+    if (mins > 59) {
+        hours = Math.floor(mins % 60);
+        if (hours < 10) {
+            hours = '0' + String(hours);
+        }
+    }
+
     if (mins < 10) {
         mins = '0' + String(mins);
     }
@@ -46,7 +98,12 @@ function updateProgress() {
         secs = '0' + String(secs);
     }
 
-    timestamp.innerHTML = `${mins}:${secs}`;
+    if (hours !== undefined) {
+        timestamp.innerHTML = `${hours}:${mins}:${secs}`;
+    } else {
+        timestamp.innerHTML = `${mins}:${secs}`;
+    }
+    
 }
 
 
@@ -55,6 +112,18 @@ function setVideoProgress() {
     video.currentTime = (+progress.value * video.duration) / 100;
 }
 
+function setVideoVolume() {
+    video.volume = +volume.value;
+}
+
+function muteVideo() {
+    if (video.volume > 0) {
+        tempVolume = video.volume;
+        video.volume = 0;
+    } else {
+        video.volume = tempVolume;
+    }
+}
 
 //  Stop video
 function stopVideo() {
@@ -133,12 +202,19 @@ function stopCam() {
     stop.alt = 'Stop';
     stop.title = 'Stop Video Playback';
 }
+
+
+function seekBackward () {
+    return (video.duration * 0.05);
+}
 // Event Listeners
 
+video.volume = startVolume;
 video.addEventListener('click', toggleVideoStatus);
 video.addEventListener('pause', updatePlayIcon);
 video.addEventListener('play', updatePlayIcon);
 video.addEventListener('timeupdate', updateProgress);
+video.addEventListener('volumechange', updateVolume);
 video.addEventListener('contextmenu', e => {
     e.preventDefault();
 });
@@ -149,5 +225,7 @@ stop.addEventListener('click', stopVideo);
 stop.addEventListener('click', stopCam);
 
 progress.addEventListener('change', setVideoProgress);
+volume.addEventListener('change', setVideoVolume);
+mute.addEventListener('click', muteVideo);
 camera.addEventListener('click', startCam);
 expand.addEventListener('click', openFullscreen);
